b25dacdf6904641c958dbdf51a767d27379259d2
juanlof-dev <contactojuanlof@gmail.com>
2026-02-07 06:29:11 +0100

    Add files via upload

---
+++ database.py
@@ -0,0 +1,35 @@
+import sqlite3
+import json
+
+DB_FILE = "accounts.db"
+
+def get_conn():
+    return sqlite3.connect(DB_FILE)
+
+def init_db():
+    with get_conn() as conn:
+        conn.execute("""
+        CREATE TABLE IF NOT EXISTS accounts (
+            user_id TEXT PRIMARY KEY,
+            data TEXT NOT NULL
+        )
+        """)
+
+def load_data():
+    with get_conn() as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT user_id, data FROM accounts")
+        rows = cur.fetchall()
+
+    return {uid: json.loads(data) for uid, data in rows}
+
+def save_data(data):
+    with get_conn() as conn:
+        cur = conn.cursor()
+        for uid, accs in data.items():
+            cur.execute("""
+            INSERT INTO accounts (user_id, data)
+            VALUES (?, ?)
+            ON CONFLICT(user_id)
+            DO UPDATE SET data=excluded.data
+            """, (uid, json.dumps(accs)))
